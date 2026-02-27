e1b765e03d5fe6be90f1b6c6debb7d4c67ab929c
unknown <mutheedavid12@gmail.com>
2026-02-27 18:42:24 +0300

    fix: fixes unused 'react' variable

--- apps/web/components/Dashboard/Pages/Payments/SubComponents/DiscountFormModal.tsx
+++ apps/web/components/Dashboard/Pages/Payments/SubComponents/DiscountFormModal.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import { useOrg } from '@components/Contexts/OrgContext'
