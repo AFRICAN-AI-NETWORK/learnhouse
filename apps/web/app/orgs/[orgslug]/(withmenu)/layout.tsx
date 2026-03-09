5594b89e8e1a967f0c9ce738e71e42cbbb9c31f9
unknown <mutheedavid12@gmail.com>
2026-03-09 22:20:03 +0300

    feat(chat-system): adds floating chat button and floating chat widget for global access

--- apps/web/app/orgs/[orgslug]/(withmenu)/layout.tsx
+++ apps/web/app/orgs/[orgslug]/(withmenu)/layout.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from 'next-auth/react'
 import { OrgMenu } from '@components/Objects/Menus/OrgMenu'
 import { NotificationProvider } from '@components/Contexts/NotificationContext'
 import { GlobalChatProvider } from '@components/Contexts/GlobalChatContext'
+import FloatingChatWidget from '@components/Objects/FloatingChatWidget'
 
 export default function RootLayout(props: {
   children: React.ReactNode
@@ -23,6 +24,7 @@ export default function RootLayout(props: {
             <main className="flex-1 min-h-0 w-full overflow-y-auto overflow-x-hidden scrollbar-hide">
               {children}
             </main>
+            <FloatingChatWidget />
           </GlobalChatProvider>
         </NotificationProvider>
       </SessionProvider>
