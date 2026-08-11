2887f79f53a243e0e06427848b45432800798d72
Olakunle Paul <111250016+OlakunlePaul@users.noreply.github.com>
2026-08-11 03:56:12 +0100

    chore: ignore and remove testing scripts from tracking

--- find.py
+++
@@ -1,8 +0,0 @@
-import tokenize
-with open('apps/api/src/services/referrals/payouts.py', 'rb') as f:
-    try:
-        for tok in tokenize.tokenize(f.readline):
-            if tok.type == tokenize.STRING and '"""' in tok.string:
-                print(tok.start[0], tok.end[0])
-    except Exception as e:
-        print(e)
